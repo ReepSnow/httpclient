5713ac369115fb52721d2d28793a48f5ac31ded0
wpt15303748401@126.com <ReepSnow>
2016-12-12 19:40:41 +0800

    添加有关定时清理无效链接的代码，以及注解

--- src/main/java/cn/snow/httpclient/IdleConnectionEvictor.java
+++ src/main/java/cn/snow/httpclient/IdleConnectionEvictor.java
@@ -7,9 +7,11 @@ public class IdleConnectionEvictor extends Thread {
     private final HttpClientConnectionManager connMgr;
 
     private volatile boolean shutdown;
+    //waitTime该变量用来控制连接的时间，防止连接不响应时导致连接长时间存在浪费资源
     private int waitTime; 
     public IdleConnectionEvictor(HttpClientConnectionManager connMgr,int waitTime) {
         this.connMgr = connMgr;
+        this.waitTime = waitTime;
         // 启动当前线程
         this.start();
     }
@@ -28,7 +30,7 @@ public class IdleConnectionEvictor extends Thread {
             // 结束
         }
     }
-
+//一个连接请求结束后，会调用该方法，唤醒run方法里面的wait()方法关闭连接，如果连接一直不结束，wait方法会等waitTime毫秒，然后直接关闭该链接
     public void shutdown() {
         shutdown = true;
         System.out.println("清理连接");
